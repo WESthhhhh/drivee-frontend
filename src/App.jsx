6a9f14e1a81fdfc30c3ff6af834732b9a6d2e32a
WESthhhhh <ahmedkhadila33@gmail.com>
2025-03-25 23:13:29 +0000

    inputsform

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Button from "./components/button";
-import star from "./assets/star.png"
-import arrow from "./assets/arrow.png"
-import add from "./assets/add.png"
-import { PrimaryInput, SecondaryInput, DisableInput, TextArea, FileInput } from "./components/formInputs";
+import star from "./assets/star.png";
+import arrow from "./assets/arrow.png";
+import add from "./assets/add.png";
+import {
+  PrimaryInput,
+  SecondaryInput,
+  DisableInput,
+  TextArea,
+  FileInput,
+} from "./components/formInputs";
 
 function App() {
   return (
@@ -20,17 +26,25 @@ function App() {
       <Button icon={add} type="primary" />
       <Button icon={add} disabled />
       <div className="container mx-auto p-8">
-        {/* Your existing button code */}
-
+        
         <h2 className="text-2xl font-bold mb-6">Form Inputs</h2>
         <from className="w-[500px]">
           <PrimaryInput label="Primary Input" placeholder="Enter your name" />
-          <SecondaryInput label="Secondary Input" placeholder="Enter your email" />
-          <DisableInput label="Disabled Input" placeholder="This input is disabled" />
-          <PrimaryInput label="Primary Input" placeholder="Enter your name" error="message error"/>
+          <SecondaryInput
+            label="Secondary Input"
+            placeholder="Enter your email"
+          />
+          <DisableInput
+            label="Disabled Input"
+            placeholder="This input is disabled"
+          />
+          {/*
+          <PrimaryInput label="Primary Input" placeholder="Enter your name" error="message error"/> 
           <PrimaryInput label="Primary Input" placeholder="Enter your name" succes="message success"/>
+          */}
+          <FileInput label="file" />
           <TextArea label="Textarea" placeholder="Enter your message" />
-          <FileInput label="file"/>
+
           <Button Children="submit" type="primary" />
         </from>
       </div>
