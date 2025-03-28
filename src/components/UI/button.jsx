6c75da6afb92bb47a5908370e9e3235c0db17c0d
isimane <simaneid05@gmail.com>
2025-03-28 14:13:51 +0000

    Refactor UI components: Button and FormInputs

--- src/components/UI/button.jsx
+++ src/components/UI/button.jsx
@@ -1,10 +1,10 @@
 import React, { Children } from 'react';
 
 const Button = ({ Children, type ,icon, disabled }) => {
-  let className = `text-[14px] md:text-[16px] px-[20px] py-[5px] rounded-[12px] hover:shadow-lg`;
+  let className = `text-[14px] md:text-[16px] px-[20px] py-[5px] rounded-small-md hover:shadow-primary-4 transition-all duration-300`;
 
   if (icon && !Children) {
-    className = `px-[10px] py-[8px]  rounded-[12px] hover:shadow-lg`;
+    className = `px-[10px] py-[8px]  rounded-small-md hover:shadow-primary-4 transition-all duration-300`;
   }
   if (disabled && icon) {
     className += ` bg-[#D1D5DB] text-[#7F8286] flex flex gap-[8px] bg-[#D1D5DB] text-[#7F8286] `;
@@ -13,15 +13,15 @@ const Button = ({ Children, type ,icon, disabled }) => {
   }
 
   if (type === "primary" && icon) {
-    className += ` bg-[#0B247A] text-[#FDFDFD] hover:bg-[#09206A] flex flex gap-[8px] hover:bg-[#09206A] hover:shadow-lg`;
+    className += ` bg-primary text-[#FDFDFD] hover:bg-[#09206A] flex flex gap-[8px] hover:bg-[#09206A] hover:shadow-primary-4 transition-all duration-300`;
   } if (type === "primary" ){
-    className += ` bg-[#0B247A] text-[#FDFDFD] hover:bg-[#09206A] `
+    className += ` bg-primary text-[#FDFDFD] hover:bg-[#09206A] `
   }
 
   if (type === "secondary" && disabled) {
-    className += `border-[1px] border-[#929599] bg-white  text-[#7F8286] `;
+    className += `border-[1px] border-[#929599] bg-light  text-[#7F8286] `;
   }if (type === "secondary") {
-    className += ` border-[1px] border-[#0B247A] text-[#0B247A]`;
+    className += ` border-[1px] border-primary text-primary`;
   }
 
   if(type === "ghost" && disabled) {
--- src/components/UI/formInputs.jsx
+++ src/components/UI/formInputs.jsx
@@ -8,7 +8,7 @@ export const PrimaryInput = ({ label, placeholder, type = "text", icon = null, e
                 <input
                     type={type}
                     placeholder={placeholder}
-                    className={`w-full p-2 border    ${error ? 'border-error' : succes ? 'border-success' : 'border-blueB50'} rounded-md focus:outline-none focus:ring-thin focus:ring-blueB75 focus:border-primary`} />
+                    className={`w-full p-2 border    ${error ? 'border-error' : succes ? 'border-success' : 'border-b50'} rounded-md focus:outline-none focus:ring-thin focus:ring-b75 focus:border-primary`} />
                 {icon && (
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <img src={`./assets/${icon}.png`} alt={icon} className="w-4 h-4" />
@@ -33,7 +33,7 @@ export const SecondaryInput = ({ label, placeholder, type = "text", icon = null 
                 <input
                     type={type}
                     placeholder={placeholder}
-                    className="w-full p-2 border border-blueB50 rounded-md focus:outline-none focus:ring-thin focus:ring-blueB75 focus:border-primary"
+                    className="w-full p-2 border border-b50 rounded-md focus:outline-none focus:ring-thin focus:ring-b75 focus:border-primary"
                 />
                 {icon && (
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -70,7 +70,7 @@ export const FileInput = ({ label }) => {
     return (
         <div className="mb-4 w-[500px]">
             <label className="block text-sm font-medium mb-[12px] text-primary">{label}</label>
-            <div className="relative flex items-center border border-blueB50 rounded-md px-3 py-2 focus-within:ring-thin focus-within:ring-blueB75 focus-within:border-primary ">
+            <div className="relative flex items-center border border-b50 rounded-md px-3 py-2 focus-within:ring-thin focus-within:ring-b75 focus-within:border-primary ">
                 <label htmlFor="file-upload" className="flex items-center cursor-pointer gap-[12px]">
                     <svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7.33317 0.333374H1.99984C1.2665 0.333374 0.673171 0.933374 0.673171 1.66671L0.666504 12.3334C0.666504 13.0667 1.25984 13.6667 1.99317 13.6667H9.99984C10.7332 13.6667 11.3332 13.0667 11.3332 12.3334V4.33337L7.33317 0.333374ZM1.99984 12.3334V1.66671H6.6665V5.00004H9.99984V12.3334H1.99984Z" fill="#7D838B" />
@@ -93,7 +93,7 @@ export const TextArea = ({ label, placeholder }) => {
             <label className="block text-sm font-medium mb-[12px] text-primary">{label}</label>
             <textarea
                 placeholder={placeholder}
-                className="w-full p-2 border border-blueB50 rounded-md h-24 focus:outline-none focus:ring-thin focus:ring-blueB75 focus:border-primary"
+                className="w-full p-2 border border-b50 rounded-md h-24 focus:outline-none focus:ring-thin focus:ring-b75 focus:border-primary"
             />
         </div>
     );
